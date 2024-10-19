5a7432d11861d3570efb964e2738a8c2201cf0a8
Tim Morgenstern <tim@infisc.com.br>
2024-10-19 12:08:00 -0300

    Criada estrutura basica do backend

---
+++ backend/src/main/java/org/sos/animais/gestao/SistemaDeGestaoSosAnimaisApplication.java
@@ -0,0 +1,13 @@
+package org.sos.animais.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoSosAnimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoSosAnimaisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/sos/animais/gestao/SistemaDeGestaoSosAnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sos.animais.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoSosAnimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
